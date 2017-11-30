cd1d11effb33e11e61fd16c5992cdc6bae6ccaf3
Jay Warner <jay0828@vt.edu>
2017-11-30 01:54:59 +0000

    first commit

---
+++ led.py
@@ -0,0 +1,137 @@
+from flask import Flask, request
+import RPi.GPIO as GPIO
+import time
+import zeroconf
+import socket
+
+app = Flask(__name__)
+
+# Module level constants
+#Globals
+RED = 5
+GREEN = 19
+BLUE = 26
+r = None
+g = None
+b = None
+
+# Sets up pins as outputs
+def setup(*leds):
+    global r
+    global g
+    global b
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    for led in leds:
+        GPIO.setup(led, GPIO.OUT)
+        GPIO.output(led, GPIO.LOW)
+    f = 100
+    r = GPIO.PWM(RED, f)
+    g = GPIO.PWM(GREEN, f)
+    b = GPIO.PWM(BLUE, f)
+    return None
+
+# Setup leds
+setup(RED, GREEN, BLUE)
+
+
+@app.route('/LED')
+def LED():
+    if(status == 'on'):
+        if(color == 'red'):
+            red_on(int(intensity))
+        elif(color == 'green'):
+            green_on(int(intensity))
+        elif(color == 'blue'):
+            blue_on(int(intensity))
+        elif(color == 'magenta'):
+            magenta_on(int(intensity))
+        elif(color == 'cyan'):
+            cyan_on(int(intensity))
+        elif(color == 'yellow'):
+            yellow_on(int(intensity))
+        elif(color == 'white'):
+            white_on(int(intensity))
+        else:
+            print("Error: unknown color, color = ", color)
+    elif(status == 'off'):
+        turn_off()#turn off LED
+    else:
+        print("Error: unknown status, status = ", status)
+    return None
+
+def turn_off():
+    r.start(0)
+    b.start(0)
+    g.start(0)
+    return None
+
+def red_on(intensity):
+    turn_off()
+    #turn on red
+    r.start(int(intensity))
+    return None
+
+def blue_on(intensity):
+    #turn off green and red
+    turn_off()
+    #turn on blue
+    b.start(int(intensity))
+    return None
+
+def green_on(intensity):
+    #turn off blue and red
+    turn_off()
+    #turn on green
+    g.start(int(intensity))
+    return None
+
+def magenta_on(intensity):
+    #turn off green
+    turn_off()
+    #turn on blue and red
+    r.start(int(intensity))
+    b.start(int(intensity))
+    return None
+
+def cyan_on(intensity):
+    #turn off red
+    turn_off()
+    #turn on blue and green
+    b.start(int(intensity))
+    g.start(int(intensity))
+    return None
+
+def yellow_on(intensity):
+    #turn off blue
+    turn_off()
+    #turn on red and green
+    r.start(int(intensity))
+    g.start(int(intensity))
+    return None
+
+def white_on(intensity):
+    #turn on all
+    r.start(int(intensity))
+    b.start(int(intensity))
+    g.start(int(intensity))
+    return None
+    
+
+if __name__ == '__main__':
+    host = '172.29.81.174'
+    info = zeroconf.ServiceInfo("_http._tcp.local.",
+                       "LED PI._http._tcp.local.",
+                       socket.inet_aton("172.29.81.174"), 800, 0, 0,
+                       {'Available Colors': "'red', 'blue', 'green', 'yellow', 'cyan', 'magenta', 'white'"}, "ash-2.local.")
+    zconf = zeroconf.Zeroconf()
+    zconf.register_service(info)
+    app.run(host, port=800, debug=False)
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        zconf.unregister_service(info)
+        zconf.close()
---
+++ services.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+from flask_httpauth import HTTPBasicAuth
+import led
+import requests
+from led import red_on, blue_on, green_on, magenta_on, yellow_on, cyan_on, white_on
+import zeroconf
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'accounts'
+app.config['MONGO_URI'] = 'mongodb://localhost/accounts'
+
+mongo = PyMongo(app)
+
+auth = HTTPBasicAuth();
+
+@auth.get_password
+def get_pw(username):
+    accounts = mongo.db.accounts
+    accExists = accounts.find_one({'username': username})
+    if accExists:
+       return accExists['password'];
+    return None
+
+@app.route('/LED')
+def handleLED():
+    status = request.args.get('status')
+    color = request.args.get('color')
+    intensity = request.args.get('intensity')
+    zc = zeroconf.Zeroconf()
+    info = zc.get_service_info("_http._tcp.local.", "LED PI._http._tcp.local.")
+    payload = {'color': color, 'status': status, 'intensity' : intensity}
+    zc = requests.get('http://172.29.81.174:5000/LED', params=payload)
+    return None
+
+
+
+@app.route('/accounts', methods=['GET'])
+@auth.login_required
+def get_all_accounts():
+    accounts = mongo.db.accounts
+    output = []
+    for s in accounts.find():
+        output.append({'username': s['username'], 'password': s['password']})
+    return jsonify({'Get all accounts': output})
+
+@app.route('/accounts/<username>', methods=['GET'])
+@auth.login_required
+def get_one_accounts(username):
+    accounts = mongo.db.accounts
+    accExists = accounts.find_one({'username': username})
+    if accExists:
+        output = {'username': accExists['username'], 'password': accExists['password']}
+    else:
+        output = "Account does not exist"
+    return jsonify({'Get account': output})
+
+
+@app.route('/accounts', methods=['POST'])
+@auth.login_required
+def add_account():
+    accounts = mongo.db.accounts
+    username = request.values.get('username')
+    password = request.values.get('password')
+    accExists = accounts.find_one({'username': username})
+    if accExists:
+        output = "User already exists"
+    else:
+        accounts.insert_one({'username': username, 'password': password})
+        accExists = accounts.find_one({'username': username, 'password': password})
+        if accExists:
+            output = {'username': accExists['username'], 'password': accExists['password']}
+        else:
+            output = "Unable to create account"
+    return jsonify({'Created': output})
+
+@app.route('/accounts', methods=['PUT'])
+@auth.login_required
+def update_password():
+    accounts = mongo.db.accounts
+    username = request.values.get('username')
+    password = request.values.get('password')
+    accExists = accounts.find_one({'username': username, 'password': password})
+    if accExists:
+        accounts.update_one({'username': username}, {'$set': {'password': password}}, upsert=False)
+        updated_account = accounts.find_one({'username': username, 'password': password})
+        if updated_account:
+            output = {'username': updated_account['username'], 'password': updated_account['password']}
+        else:
+            output = "Unable to update password"
+    else:
+        output = "Account does not exist"
+    return jsonify({'Updated': output})
+
+@app.route('/letmein')
+def printIn():
+    print("made it")
+    
+@app.route('/accounts', methods=['DELETE'])
+@auth.login_required
+def remove_account():
+    accounts = mongo.db.accounts
+    username = request.values.get('username')
+    password = request.values.get('password')
+    accExists = accounts.find_one({'username': username, 'password': password})
+    if accExists:
+        accounts.remove({'username': username, 'password': password})
+        removed_account = accounts.find_one({'username': username})
+        if removed_account:
+            output = "Unable to delete account"
+        else:
+            output = {'username': username, 'password': password}
+    else:
+        output = "Username and password do not match or account does not exist"
+    return jsonify({'Deleted': output})
+
+if __name__ == '__main__':
+    app.run(host='172.29.81.174', port=800, debug=True)
+    
